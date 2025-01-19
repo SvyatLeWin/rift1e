4576d0262b9b9015927911108549d374a17d545d
SvyatLeWin <jp3103@mail.ru>
2025-01-19 08:15:59 +0300

    s

--- module/sheets/rift1eItemSheet.js
+++ module/sheets/rift1eItemSheet.js
@@ -1,6 +1,6 @@
 export default class rift1eItemSHeet extends ItemSheet{
     get template(){
-        return `systems/rift1e/templates/sheets/${this.item.data.type}-sheet.html`;
+        return `systems/rift1e/templates/sheets/${this.item.data.type}-sheet.hbs`;
     }
     getData(){
         const baseData= super.getData();
