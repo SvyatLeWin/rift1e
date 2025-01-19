46cc3e51da09a272dca92f414fdc8d4718178091
SvyatLeWin <jp3103@mail.ru>
2025-01-19 08:11:43 +0300

    s

--- module/sheets/rift1eItemSheet.js
+++ module/sheets/rift1eItemSheet.js
@@ -3,9 +3,15 @@ export default class rift1eItemSHeet extends ItemSheet{
         return `systems/rift1e/templates/sheets/${this.item.data.type}-sheet.html`;
     }
     getData(){
-        const data= super.getData();
-        data.config=CONFIG.rift1e;
-        return data;
+        const baseData= super.getData();
+        let sheetData={
+            owner:this.item.isOwner,
+            editable:this.isEditable,
+            item:baseData.item,
+            data:baseData.item.data.data,
+            data.config=CONFIG.rift1e
+        };
+        return sheetData;
     }
 }
 //Finall?
