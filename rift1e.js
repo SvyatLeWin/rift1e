3667c8d9d40accc6a50b0f69ab7296187ee8ef4b
SvyatLeWin <jp3103@mail.ru>
2025-01-19 08:40:36 +0300

    s

--- rift1e.js
+++ rift1e.js
@@ -2,14 +2,15 @@ import { rift1e } from "./module/config";
 import rift1eItemSHeet from "./module/sheets/rift1eItemSheet.js";
 
 Hooks.once("init", function(){
-    Gamepad.rift1e={
+    game.rift1e={
         rift1eActor,
         rift1eItem,
         rollItemMacro
     },
     console.log("rift1e | Initialising Rift system");
     CONFIG.rift1e=rift1e;
-    
+    CONFIG.Item.documentClass = rift1eItem;
+    CONFIG.Actor.documentClass = rift1eActor;
     Items.unregisterSheet("core",ItemSheet);
     Items.registerSheet("rift1e",rift1eItemSHeet,{ 
         makeDefault: true,
