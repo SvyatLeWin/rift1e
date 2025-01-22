6b279025741fc52dee1d36c2176dfd7e51d9b684
SvyatLeWin <jp3103@mail.ru>
2025-01-22 14:14:16 +0300

    s

--- module/sheets/rift1eItemSheet.js
+++ module/sheets/rift1eItemSheet.js
@@ -1,24 +1,24 @@
 export default class rift1eItemSHeet extends ItemSheet{
-    static get defaultOptions(){
-        return mergeObject(super.defaultOptions,{
-            width: 530,
-            height: 340,
-            classes:["rift1e","sheet","item"]
-            });
+    // static get defaultOptions(){
+    //     return mergeObject(super.defaultOptions,{
+    //         width: 530,
+    //         height: 340,
+    //         classes:["rift1e","sheet","item"]
+    //         });
         
-    }
+    // }
     get template(){
         return `systems/rift1e/templates/sheets/${this.item.data.type}-sheet.html`;
     }
-    getData(){
-        const baseData= super.getData();
-        let sheetData={
-            owner:this.item.isOwner,
-            editable:this.isEditable,
-            item:baseData.item,
-            data:baseData.item.data.data,
-            config: CONFIG.rift1e
-        };
-        return sheetData;
-    }
+    // getData(){
+    //     const baseData= super.getData();
+    //     let sheetData={
+    //         owner:this.item.isOwner,
+    //         editable:this.isEditable,
+    //         item:baseData.item,
+    //         data:baseData.item.data.data,
+    //         config: CONFIG.rift1e
+    //     };
+    //     return sheetData;
+    // }
 }
