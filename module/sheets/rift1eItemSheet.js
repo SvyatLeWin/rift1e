eedaac3646d98a85a8ab00f55555579d04bcac6e
SvyatLeWin <jp3103@mail.ru>
2025-01-22 14:26:57 +0300

    s

--- module/sheets/rift1eItemSheet.js
+++ module/sheets/rift1eItemSheet.js
@@ -10,7 +10,7 @@ export default class rift1eItemSHeet extends ItemSheet{
     get template(){
         console.log("this.item",this.item);
 
-        return `systems/rift1e/templates/sheets/${this.item.data.type}-sheet.html`;
+        return `systems/rift1e/templates/sheets/${this.item.type}-sheet.html`;
     }
     // getData(){
     //     const baseData= super.getData();
