b27dc8312746373b77f9c36d56096cf5a22155f8
SvyatLeWin <jp3103@mail.ru>
2025-01-22 14:19:18 +0300

    s

--- rift1e.js
+++ rift1e.js
@@ -2,15 +2,15 @@ import { rift1e } from "./module/config.js";
 import rift1eItemSHeet from "./module/sheets/Rift1eItemSheet.js";
 
 Hooks.once("init", function(){
-    game.rift1e={
-        rift1eActor,
-        rift1eItem,
-        rollItemMacro
-    },
+    // game.rift1e={
+    //     rift1eActor,
+    //     rift1eItem,
+    //     rollItemMacro
+    // },
     console.log("rift1e | Initialising Rift system");
-    CONFIG.rift1e=rift1e;
-    CONFIG.Item.documentClass = rift1eItem;
-    CONFIG.Actor.documentClass = rift1eActor;
+    // CONFIG.rift1e=rift1e;
+    // CONFIG.Item.documentClass = rift1eItem;
+    // CONFIG.Actor.documentClass = rift1eActor;
     Items.unregisterSheet("core",ItemSheet);
     Items.registerSheet("rift1e",rift1eItemSHeet,{makeDefault: true});
 });
