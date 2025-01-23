d019693870888ca007c8700040d0759c8ac7531d
SvyatLeWin <jp3103@mail.ru>
2025-01-23 11:22:26 +0300

    s

--- module/config.js
+++ module/config.js
@@ -3,5 +3,5 @@ rift1e.attackTypes={
     none:"",
     fencing:"rift1e.attack.fencing",
     shooting:"rift1e.attack.shooting",
-    aesperForce:"rift1e.a-ttack.aesperForce"
+    aesperForce:"rift1e.attack.aesperForce"
 }
