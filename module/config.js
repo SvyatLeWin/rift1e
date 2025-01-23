28dea466f78e0aad8b0c87cf8bc5b99e1cbba43a
SvyatLeWin <jp3103@mail.ru>
2025-01-23 13:48:00 +0300

    ы

--- module/config.js
+++ module/config.js
@@ -5,3 +5,9 @@ rift1e.attackTypes={
     shooting:"rift1e.attack.shooting",
     aesperForce:"rift1e.attack.aesperForce"
 }
+rift1e.weaponTypes={
+    none:"",
+    light:"rift1e.weaponType.light",
+    medium:"rift1e.weaponType.medium",
+    heavy:"rift1e.weaponType.heavy"
+}
