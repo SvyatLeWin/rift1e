c717ff1ece1767711a998915651aec3e7e2c46e5
SvyatLeWin <jp3103@mail.ru>
2025-01-23 14:13:26 +0300

    s

--- module/config.js
+++ module/config.js
@@ -1,13 +1,14 @@
 export const rift1e = {};
-rift1e.attackTypes=[];
-rift1e.attackTypes["none"] = "";
-rift1e.attackTypes["fencing"] = "rift1e.attack.fencing";
-rift1e.attackTypes["shooting"] = "rift1e.attack.shooting";
-rift1e.attackTypes["aesperForce"] = "rift1e.attack.aesperForce";
-
+rift1e.attackTypes={
+    none:"",
+    fencing:"rift1e.attack.fencing",
+    shooting:"rift1e.attack.shooting",
+    aesperForce:"rift1e.attack.aesperForce"
+}
 rift1e.weaponTypes={
     none:"",
     light:"rift1e.weaponType.light",
     medium:"rift1e.weaponType.medium",
     heavy:"rift1e.weaponType.heavy"
 }
+rift1e.selectedAttackType= "none"
